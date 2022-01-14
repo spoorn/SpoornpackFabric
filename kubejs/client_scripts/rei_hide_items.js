8ce50e8598173e1e58aa5aca14ef34777e3e34d5
spoorn <michael.trinh1996@gmail.com>
2022-01-14 13:44:25 -0800

    Initial release 0.1

---
+++ kubejs/client_scripts/rei_hide_items.js
@@ -0,0 +1,7 @@
+events.listen('rei.hide.items', event => {
+    console.log("Hiding extra slime sling/boots");
+    event.hide('kibe:slime_sling');
+    event.hide('equip:slime_boots');
+
+    event.hide('techreborn:sapphire_sword');
+})
