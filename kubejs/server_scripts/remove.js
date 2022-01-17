3ab6a95c075d8bcdf808e5de55d56d738cd821e2
spoorn <spookump@gmail.com>
2022-01-16 16:58:26 -0800

    0.3 release
    First release on curseforge

--- kubejs/server_scripts/remove.js
+++
@@ -1,8 +0,0 @@
-// remove items
-events.listen('recipes', function (event) {
-
-    event.remove({ output: "techreborn:sapphire_sword" });
-
-    // Causes server kick due to allow-flight=false in server.properties by default
-    event.remove({ output: 'gobber2:gobber2_ring_airwalking' });
-})
--- kubejs/server_scripts/tooltips.js
+++
@@ -1,6 +0,0 @@
-events.listen("rei.information", function (event) {
-
-  event.add("minecraft:stone", ['Line 1', 'Line 2'])
-
-
-});
